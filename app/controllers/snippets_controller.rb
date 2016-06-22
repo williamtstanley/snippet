6a581951d4760ff0a4925be79ceb0676f9fd8b3f
Eric Richardson <eric@ericonrails.com>
2016-06-21 18:30:31 -0700

    Add #new and #create for snippet

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,2 +1,20 @@
 class SnippetsController < ApplicationController
+  def new
+    @snippet = Snippet.new
+  end
+
+  def create
+    snippet = Snippet.new snippet_params
+    if snippet.save
+      redirect_to snippet_path(snippet)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def snippet_params
+      params.require(:snippet).permit(:title, :work, :private)
+    end
 end
--- spec/controllers/snippets_controller_spec.rb
+++ spec/controllers/snippets_controller_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe SnippetsController, type: :controller do
+  describe "#new" do
+    before {get :new}
+
+    it "should render the new template" do
+      expect(response).to render_template(:new)
+    end
+
+    it "should instantiate a variable equal to a new snippet" do
+      expect(assigns(:snippet)).to be_a_new(Snippet)
+    end
+  end
+
+  describe "#create" do
+    context "with valid paramaters" do
+      def valid_request
+        post :create, snippet: (attributes_for(:snippet))
+      end
+
+      it "should add a new snippet to the database" do
+        count_before = Snippet.count
+        valid_request
+        count_after = Snippet.count
+        expect(count_after).to eq(count_before + 1)
+      end
+
+      it "should redirect to show snippet page" do
+        valid_request
+        expect(response).to redirect_to(snippet_path(Snippet.last))
+      end
+    end
+
+    context "with invalid paramaters" do
+      def invalid_request
+        post :create, snippet: (attributes_for(:snippet, title: ''))
+      end
+
+      it "should not add a snippet to the database" do
+        count_before = Snippet.count
+        invalid_request
+        count_after = Snippet.count
+        expect(count_after).to eq(count_before)
+      end
+
+      it "should render the new template" do
+        invalid_request
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 
 end
