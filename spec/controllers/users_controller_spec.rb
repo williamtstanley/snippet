4ec7efdbe19b7df4576e0868397430b10e673493
William Stanley <william.t.stanley@gmail.com>
2016-06-21 18:06:56 -0700

    User controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "#new" do
+    before {get :new}
+    it {expect(response).to render_template(:new)}
+    it {expect(assigns(:user)).to be_a_new(User)}
+  end
 
 end
