32241400fb9ee849486558d13196dc20806cc522
William Stanley <william.t.stanley@gmail.com>
2016-06-21 17:56:39 -0700

    User Validation tests and Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  VALID_EMAIL_REGEX = /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :email, format: VALID_EMAIL_REGEX, uniqueness: true
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,10 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "validations" do
     it "confirms that there is a first name" do
-      u =
+      u = build(:user, {first_name: nil})
+      u.valid?
+      expect(u.errors).to have_key(:first_name)
     end
-    it "confirms that there is a last name"
-    it "confirms that there is a valid email address"
-    it "confirms that the email address is unique"
+    it "confirms that there is a last name" do
+      u = build(:user, {last_name: nil})
+      u.valid?
+      expect(u.errors).to have_key(:last_name)
+    end
+    it "confirms that there is a valid email address" do
+      u = build(:user, {email: "I am an invalid email address"})
+      u.valid?
+      expect(u.errors).to have_key(:email)
+    end
+    it "confirms that the email address is unique" do
+      create(:user, {email: "duder@example.com"})
+      u = build(:user, {email: "duder@example.com"})
+      u.valid?
+      expect(u.errors).to have_key(:email)
+    end
   end
 end
