9ca39aa6955156a50218a7a9ca395efb3b5bc44c
William Stanley <william.t.stanley@gmail.com>
2016-06-21 17:46:24 -0700

    flesh out test suite

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "confirms that there is a first name" do
+      u =
+    end
+    it "confirms that there is a last name"
+    it "confirms that there is a valid email address"
+    it "confirms that the email address is unique"
+  end
 end
